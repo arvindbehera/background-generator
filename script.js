6e40a58489289a07e628048513658024b69a2ebf
arvindbehera <arvind.behera@yahoo.co.in>
2019-09-14 22:16:55 +0530

    Adding project

---
+++ script.js
@@ -0,0 +1,12 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("body1")
+
+color1.addEventListener("input", function(){
+    body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
+})
+
+color2.addEventListener("input", function() {
+  body.style.background =
+    "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
+});
